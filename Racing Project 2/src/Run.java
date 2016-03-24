ca8e763aa2df945af3a4bc5b6191d0a405e102eb
Tommy Wilson <wilso237@uwm.edu>
2016-03-24 17:47:44 -0500

    Filled in Run stubs

--- Racing Project 2/src/Run.java
+++ Racing Project 2/src/Run.java
@@ -9,19 +9,19 @@ public class Run {
 	private boolean channels[];
 	private int runNum;
 	
-	public Run(int type, int runNum){}
-	public boolean setType(int type){}
-	public int getType(){}
-	public int getRunNum(){}
+	public Run(int type, int runNum){this.type=type;this.runNum=runNum;}//TODO?
+	public boolean setType(int type){this.type=type;return true;}//TODO?
+	public int getType(){return type;}//TODO?
+	public int getRunNum(){return runNum;}//TODO?
 	public boolean setTime (String time){
 		return this.time.stringToTime(time);
 	}
-	public boolean setTime (int H, int M, int S, int mS){}
-	void toggle (int channel){}
-	boolean trigger (int channel){}
-	ArrayList<Racer> getRacers (){}
-	boolean addRacer (int number){}
+	public boolean setTime (int H, int M, int S, int mS){if(time != null){return false;}time=new Time(H,M,S,mS);return true;}//TODO?
+	void toggle (int channel){channels[channel-1]=!channels[channel-1];}//TODO?
+	boolean trigger (int channel){channels[channel-1]=true;channels[channel-1]=false;return true;}//TODO
+	ArrayList<Racer> getRacers (){return racers;}//TODO?
+	boolean addRacer (int number){racers.add(new Racer(number));return true;}//TODO?
 	
-	boolean running (){}
-	void end (){}
+	boolean running (){return running;}//TODO?
+	void end (){}//TODO
 }
