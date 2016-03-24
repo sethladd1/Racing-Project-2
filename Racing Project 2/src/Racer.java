9fe184d9cd15e39e039bbe4a50506ab2e1d790af
yangyuqing <yangyuqing@10.20.67.198>
2016-03-24 13:38:13 -0500

    racer

---
+++ Racing Project 2/src/Racer.java
@@ -0,0 +1,50 @@
+
+
+public class Racer {
+	
+		private long s, f;
+		private boolean dnf; 
+
+		public int Racers (int number)
+		{
+		
+			return number;
+		}
+		public void setStart (long start)
+		{
+			s=start;
+		}
+		public void setFinish (long finish)
+		{
+			f=finish;
+		}
+		public void setDNF (boolean a)
+		{
+			dnf=a;
+		}
+		public long getStar()
+		{
+			return s;
+		}
+		public long getFinish()
+		{
+			return f;
+		}
+		public long getRunTime()
+		{
+			return f-s;
+		}
+		public boolean started()
+		{
+			return true;
+		}
+		public boolean finished()
+		{
+			return true;
+		}
+		public boolean DNF()
+		{
+			return true;
+		}
+		
+}
